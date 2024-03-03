216f3a6ce1f7152ed2191395db3a9ccdab4123fe
Otavio Costa <dev.otaviocostao@gmail.com>
2024-03-03 19:14:58 -0300

    Correção na questao 2

--- Pratica 2/Questao2.c
+++ Pratica 2/Questao2.c
@@ -22,9 +22,11 @@ int main(){
     setlocale(LC_ALL, "Portuguese");
 
     int i, condicao, encontrado=0;
-    char busca[11];
+    char busca[11], head_nome[100], head_cpf[20], head_sexo[30];
 
     printf("Cadastro de pessoas:\n\n");
+
+    //Receber as informações:
 
     for(i=0;i<TAM;i++){
         printf("Digite o nome: ");
@@ -42,26 +44,44 @@ int main(){
 
         printf("\nDigite o sexo: ");
         gets(pessoa[i].sexo);
+
+        printf("\n");
     }
+
+    //Digitar o CPF para buscar usuario:
 
     printf("\n\nDigite o CPF que deseja buscar: ");
     gets(busca);
+
+    //Realizando a busca e exibindo as informações
 
     for(i=0;i<TAM;i++){
         condicao = strcmp(busca,pessoa[i].cpf);
         if(condicao==0){
             printf("\n");
-            puts(pessoa[i].nome);
+            strcpy(head_nome, "Nome: ");
+            strcat(head_nome, pessoa[i].nome);
+            puts(head_nome);
+
             printf("%.2f", pessoa[i].altura);
+
             printf("\n%.2f", pessoa[i].peso);
             printf("\n");
-            puts(pessoa[i].cpf);
-            puts(pessoa[i].sexo);
+
+            strcpy(head_cpf, "CPF: ");
+            strcat(head_cpf, pessoa[i].cpf);
+            puts(head_cpf);
+
+            strcpy(head_sexo, "Sexo: ");
+            strcat(head_sexo, pessoa[i].sexo);
+            puts(head_sexo);
 
             encontrado = 1;
             break;
         }
     }
+
+    //Caso não encontre o CPF buscado
 
     if(encontrado!=1){
         printf("\nO CPF buscado não foi encontrado\n");
