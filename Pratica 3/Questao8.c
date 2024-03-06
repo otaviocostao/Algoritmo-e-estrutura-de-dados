5cc70f20ac5832db1e18a227d41f15b98551b4a6
Otavio Costa <dev.otaviocostao@gmail.com>
2024-03-06 14:51:43 -0300

    usando verdadeiro ou falso

--- Pratica 3/Questao8.c
+++ Pratica 3/Questao8.c
@@ -2,33 +2,26 @@
 #include <stdlib.h>
 #include <locale.h>
 
-void numero_primo(int num){
-    int i, primo =1;
+int numero_primo(int num){
+    int i;
 
     if(num<=1){
-        printf("\nO número %d não é primo\n", num);
-        return;
+        return 0; // FALSO
     }
     
     for(i=2; i*i <= num; i++){
         if(num % i == 0){
-            primo = 0;
-            break;
+            return 0; // FALSO
         }
     }
 
-    if(primo == 1){
-        printf("\nO número %d é primo\n", num);
-    }else{
-        printf("\nO número %d não é primo\n", num);
-    }
-    system("pause");
+    return 1; // VERDADEIRO
 }
 
 int main(){
     setlocale(LC_ALL, "Portuguese");
 
-    int num;
+    int num, primo;
     while(1){
         printf("\n-- Checar se o número é primo --\n\n");
 
@@ -41,7 +34,14 @@ int main(){
             break;
         }
 
-        numero_primo(num);
+        primo = numero_primo(num);
+        if(primo == 1){
+            printf("\nO número %d é primo\n", num);
+            system("pause");
+        }else{
+            printf("\nO número %d não é primo\n", num);
+            system("pause");
+        }
 
     }
 
